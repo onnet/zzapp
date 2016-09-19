8171c4cfd782d800ea413e4b3821d51a87eabbb5
Kirill Sysoev <kirill.sysoev@gmail.com>
2016-09-19 21:09:05 +0000

    update

--- src/cb_onbill_transactions.erl
+++ src/cb_onbill_transactions.erl
@@ -32,10 +32,23 @@ validate(Context, Id) ->
 
 -spec validate_periodic_fees(cb_context:context(), ne_binary(), path_token()) -> cb_context:context().
 validate_periodic_fees(Context, <<"dailyfee-", Year:4/binary, Month:2/binary, _/binary>> = Id, ?HTTP_GET) ->
-    crossbar_doc:load(Id, cb_context:set_account_modb(Context, kz_util:to_integer(Year), kz_util:to_integer(Month)), [{'expected_type', <<"debit">>}]).
+    leak_job_fields(crossbar_doc:load(Id, cb_context:set_account_modb(Context, kz_util:to_integer(Year), kz_util:to_integer(Month)), [{'expected_type', <<"debit">>}])).
 
 -spec maybe_valid_relationship(cb_context:context()) -> boolean().
 maybe_valid_relationship(Context) ->
     AccountId = cb_context:account_id(Context),
     AuthAccountId = cb_context:auth_account_id(Context),
     onbill_util:validate_relationship(AccountId, AuthAccountId) orelse cb_context:is_superduper_admin(AuthAccountId).
+
+-spec leak_job_fields(cb_context:context()) -> cb_context:context().
+leak_job_fields(Context) ->
+    case cb_context:resp_status(Context) of
+        'success' ->
+            JObj = cb_context:doc(Context),
+            cb_context:set_resp_data(Context
+                                    ,kz_json:set_values([{<<"status">>, kz_json:get_value(<<"pvt_metadata">>, JObj)}
+                                                        ], cb_context:resp_data(Context))
+                                    );
+        _Status -> Context
+    end.
+
