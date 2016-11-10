c85d9f753930df6ad6c12bd7d18d417986e2baf4
Kirill Sysoev <kirill.sysoev@gmail.com>
2016-11-10 21:59:47 +0000

    update

--- src/docs_numbering.erl
+++ src/docs_numbering.erl
@@ -1,13 +1,53 @@
 -module(docs_number).
 
 -export([get_number/5
-    %    ,lookup_number/3
+        ,number_lookup/3
         ]).
 
 -include("onbill.hrl").
 
-get_number(AccountId, _Carrier, _DocType, Year, _Month) ->
+-define(RECENT_NUMBER_VIEW, <<"doc_numbering/recent_number">>).
+-define(NUMBER_LOOKUP_VIEW, <<"doc_numbering/number_lookup">>).
+
+get_number(AccountId, Carrier, DocType, Year, Month) ->
     ResellerId = kz_services:find_reseller_id(AccountId),
     DbName = ?DOC_NUMBER_DOC(ResellerId, Year),
     _ = onbill_util:check_db(DbName),
-    <<"docs_number_13">>.
+    case number_lookup(AccountId, Carrier, DocType, Year, Month) of
+        {'ok', Number} -> {'ok', Number};
+        {'error', 'mot_found'} -> maybe_get_new_number(AccountId, Carrier, DocType, Year, Month),
+        E -> E
+    end.
+
+number_lookup(AccountId, Carrier, DocType, Year, Month) ->
+    ResellerId = kz_services:find_reseller_id(AccountId),
+    DbName = ?DOC_NUMBER_DOC(ResellerId, Year),
+    _ = onbill_util:check_db(DbName),
+    Opts = [{'startkey', [Carrier, DocType, Month, AccountId]}
+           ,{'endkey', [Carrier, DocType, Month, AccountId]}]
+    case kz_datamgr:get_results(DbName, ?NUMBER_LOOKUP_VIEW, Opts) of
+        {'ok', []} -> {'error', 'not_found'};
+        {'ok', [JObj]} -> {'ok', kz_json:get_integer_value(<<"value">>,JObj)};
+        E -> {'error', E}
+    end.
+
+get_recent_number(AccountId, Carrier, DocType, Year, _Month) ->
+    ResellerId = kz_services:find_reseller_id(AccountId),
+    DbName = ?DOC_NUMBER_DOC(ResellerId, Year),
+    _ = onbill_util:check_db(DbName),
+    Opts = [{'startkey', [<<"beeline">>,<<"invoice">>, 9]}
+           ,{'endkey', [<<"beeline">>,<<"invoice">>, 0]}
+           ,'descending'
+           ,{limit,1}
+           ],
+    case kz_datamgr:get_results(DbName, ?RECENT_NUMBER_VIEW, Opts) of
+        {'ok', []} -> {'ok', 0};
+        {'ok', [JObj]} -> {'ok', kz_json:get_integer_value(<<"value">>,JObj)};
+        E -> {'error', E}
+    end.
+
+get_new_number(AccountId, Carrier, DocType, Year, Month) ->
+    case get_recent_number(AccountId, Carrier, DocType, Year, _Month) of
+        {'ok', RecentNumber} -> {'ok', RecentNumber + 1};
+        {'error', E} -> {'error', E}
+    end.
--- src/onbill.hrl
+++ src/onbill.hrl
@@ -10,14 +10,26 @@
 -define(ONBILL_DOC, <<"onbill">>).
 -define(CARRIER_DOC(CarrierName), <<"onbill_carrier.", CarrierName/binary>>).
 -define(MOD_CONFIG_CRAWLER, <<(?APP_NAME)/binary, ".account_crawler">>).
--define(DOC_NUMBER_DOC(ResellerId, Year), <<(?APP_NAME)/binary, "-", ResellerId/binary, "-", (kz_util:to_binary(Year))/binary>>).
+-define(DOC_NUMBER_DOC(ResellerId, Year), <<(?APP_NAME)/binary
+                                           ,"-"
+                                           ,ResellerId/binary
+                                           ,"-"
+                                           ,(kz_util:to_binary(Year))/binary>>).
 -define(SYSTEM_CONFIG_DB, <<"system_config">>).
 -define(DOC_NAME_FORMAT(Carrier, TemplateID), <<Carrier/binary, "_", TemplateId/binary>>).
 -define(HTML_TO_PDF(TemplateId), <<"php applications/onbill/priv/templates/ru/", TemplateId/binary, ".php">>).
--define(HTML_TO_PDF(TemplateId, Carrier), <<"php applications/onbill/priv/templates/ru/", Carrier/binary, "_", TemplateId/binary, ".php">>).
+-define(HTML_TO_PDF(TemplateId, Carrier), <<"php applications/onbill/priv/templates/ru/"
+                                           ,Carrier/binary
+                                           ,"_"
+                                           ,TemplateId/binary
+                                           ,".php">>).
 -define(DEFAULT_REGEX, <<"^\\d*$">>).
 -define(START_DATE(Month, Year), <<"01.",(kz_util:pad_month(Month))/binary,".",(kz_util:to_binary(Year))/binary>>).
--define(END_DATE(Month, Year), <<(kz_util:to_binary(calendar:last_day_of_the_month(Year, Month)))/binary,".",(kz_util:pad_month(Month))/binary,".",(kz_util:to_binary(Year))/binary>>).
+-define(END_DATE(Month, Year), <<(kz_util:to_binary(calendar:last_day_of_the_month(Year, Month)))/binary
+                                ,"."
+                                ,(kz_util:pad_month(Month))/binary
+                                ,"."
+                                ,(kz_util:to_binary(Year))/binary>>).
 -define(ACC_CHILDREN_LIST, <<"accounts/listing_by_children">>).
 
 -define(ONBILL_HRL, 'true').
