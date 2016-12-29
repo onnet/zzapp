f933ee9934df6c3f29de05ea17b4cba0bcfe2d8b
Kirill Sysoev <kirill.sysoev@gmail.com>
2016-12-29 20:09:48 +0000

    update

--- src/onbill_handlers.erl
+++ src/onbill_handlers.erl
@@ -16,21 +16,26 @@
 
 -spec handle_doc_created(kz_json:object(), kz_proplist()) -> any().
 handle_doc_created(JObj, _Props) ->
-    lager:info("IAM listen doc_dreated JObj: ~p",[JObj]),
-    AccountId = kz_json:get_value(<<"Account-ID">>, JObj),
-    case kz_json:get_value(<<"Type">>, JObj) of
-        <<"device">> ->
-            _ = kz_services:reconcile(AccountId),
-            _ = kz_service_sync:sync(AccountId);
-        <<"user">> ->
-            _ = kz_services:reconcile(AccountId),
-            _ = kz_service_sync:sync(AccountId);
-        _ ->
-            'ok'
-    end.
+    handle_doc_created(kz_json:get_value(<<"Type">>, JObj)
+                      ,kz_json:get_value(<<"Account-ID">>, JObj)
+                      ,JObj 
+                      ).
+
+handle_doc_created(_, 'undefined', _) ->
+    'ok';
+handle_doc_created(<<"device">>, AccountId, _JObj) ->
+    _ = kz_services:reconcile(AccountId),
+    _ = kz_service_sync:sync(AccountId);
+handle_doc_created(<<"user">>, AccountId, _JObj) ->
+    _ = kz_services:reconcile(AccountId),
+    _ = kz_service_sync:sync(AccountId);
+handle_doc_created(<<"number">>, AccountId, _JObj) ->
+    _ = kz_services:reconcile(AccountId),
+    _ = kz_service_sync:sync(AccountId);
+handle_doc_created(_, _, _) ->
+    'ok'.
 
 -spec handle_logger(kz_json:object(), kz_proplist()) -> any().
 handle_logger(JObj, _Props) ->
     lager:info("IAM listen to JObj: ~p",[JObj]),
- %%   lager:info("IAM listen to Props: ~p",[Props]),
     ok.
