393a3afe35aa2ab98a95f80d2f7f82a2d9fe5c20
Kirill Sysoev <kirill.sysoev@gmail.com>
2017-02-26 20:17:19 +0000

    update

--- src/onbill_daily_sync.erl
+++ src/onbill_daily_sync.erl
@@ -116,11 +116,11 @@ process_account(AccountId, AccountJObj) ->
            andalso onbill_bk_util:today_dailyfee_absent(AccountId)
     of
         'true' ->
-            lager:debug("crawler - saving account ~s as dirty", [AccountId]),
+            lager:debug("crawler - saving account ~p (~p) as dirty", [AccountId, kz_account:name(AccountJObj)]),
             kz_services:save_as_dirty(AccountId),
             onbill_notifications:maybe_send_account_updates(AccountId, AccountJObj),
             {'ok', 'account_processed'};
         'false' ->
-            lager:debug("crawler - no need to process account ~p", [AccountId]),
+            lager:debug("crawler - no need to process account ~p (~p)", [AccountId, kz_account:name(AccountJObj)]),
             {'ok', 'no_need_to_process'}
     end.
