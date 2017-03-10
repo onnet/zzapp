321c92250ece159dc71ca0719eb876584890018f
Kirill Sysoev <kirill.sysoev@gmail.com>
2017-03-10 09:34:44 +0000

    update

--- src/crossbar/cb_onbill_proforma.erl
+++ src/crossbar/cb_onbill_proforma.erl
@@ -1,21 +1,15 @@
 -module(cb_onbill_proforma).
 
 -export([init/0
-         ,allowed_methods/0, allowed_methods/1, allowed_methods/2
-         ,resource_exists/0, resource_exists/1, resource_exists/2
-         ,content_types_provided/1, content_types_provided/2, content_types_provided/3
-         ,validate/1, validate/2, validate/3
+         ,allowed_methods/0, allowed_methods/1
+         ,resource_exists/0, resource_exists/1
+         ,content_types_provided/1, content_types_provided/2
+         ,validate/1, validate/2
         ]).
 
 -include("/opt/kazoo/applications/crossbar/src/crossbar.hrl").
 
 -define(CB_PROFORMA_INVOICE, <<"onbills/proforma_invoice">>).
--define(ATTACHMENT, <<"attachment">>).
--define(GENERATE, <<"generate">>).
--define(MODB, <<"onbills_modb">>).
--define(NOTIFICATION_MIME_TYPES, [{<<"text">>, <<"html">>}
-                               %   ,{<<"text">>, <<"plain">>}
-                                 ]).
 
 -spec init() -> 'ok'.
 init() ->
@@ -26,41 +20,29 @@ init() ->
 
 -spec allowed_methods() -> http_methods().
 -spec allowed_methods(path_token()) -> http_methods().
--spec allowed_methods(path_token(),path_token()) -> http_methods().
 allowed_methods() ->
     [?HTTP_GET,?HTTP_PUT].
 allowed_methods(_) ->
-    [?HTTP_GET].
-allowed_methods(_,_) ->
-    [?HTTP_GET].
+    [?HTTP_GET,?HTTP_DELETE].
 
 -spec resource_exists() -> 'true'.
 -spec resource_exists(path_token()) -> 'true'.
--spec resource_exists(path_token(),path_token()) -> 'true'.
 resource_exists() -> 'true'.
 resource_exists(_) -> 'true'.
-resource_exists(_,_) -> 'true'.
 
 -spec content_types_provided(cb_context:context()) -> cb_context:context().
 -spec content_types_provided(cb_context:context(), path_token()) -> cb_context:context().
--spec content_types_provided(cb_context:context(), path_token(), path_token()) -> cb_context:context().
 content_types_provided(Context) ->
     Context.
 content_types_provided(Context,_) ->
     Context.
-content_types_provided(Context,_,?ATTACHMENT) ->
-    CTP = [{'to_binary', [{<<"application">>, <<"pdf">>}]}],
-    cb_context:set_content_types_provided(Context, CTP).
 
 -spec validate(cb_context:context()) -> cb_context:context().
 -spec validate(cb_context:context(), path_token()) -> cb_context:context().
--spec validate(cb_context:context(), path_token(), path_token()) -> cb_context:context().
 validate(Context) ->
     validate_onbill_poforma(Context, cb_context:req_verb(Context)).
 validate(Context, Id) ->
     validate_onbill_poforma(Context, Id, cb_context:req_verb(Context)).
-validate(Context, Id, ?ATTACHMENT) ->
-    validate_onbill_poforma(Context, Id, ?ATTACHMENT, cb_context:req_verb(Context)).
 
 -spec validate_onbill_poforma(cb_context:context(), http_method()) -> cb_context:context().
 validate_onbill_poforma(Context, ?HTTP_GET) ->
@@ -69,10 +51,9 @@ validate_onbill_poforma(Context, ?HTTP_PUT) ->
     create(Context).
 
 validate_onbill_poforma(Context, <<Year:4/binary, Month:2/binary, _/binary>> = Id, ?HTTP_GET) ->
-    crossbar_doc:load(Id, cb_context:set_account_modb(Context, kz_term:to_integer(Year), kz_term:to_integer(Month))).
-
-validate_onbill_poforma(Context, Id, ?ATTACHMENT, ?HTTP_GET) ->
-    load_attachment(Context, Id).
+    crossbar_doc:load(Id, cb_context:set_account_modb(Context, kz_term:to_integer(Year), kz_term:to_integer(Month)));
+validate_onbill_poforma(Context, <<_Year:4/binary, _Month:2/binary, _/binary>> = Id, ?HTTP_DELETE) ->
+    mark_proforma_doc_deleted(Context, Id).
 
 -spec summary(cb_context:context()) -> cb_context:context().
 summary(Context) ->
@@ -105,29 +86,14 @@ create(Context) ->
             cb_context:add_system_error('error', Context)
     end.
 
-load_attachment(Context0, <<Year:4/binary, Month:2/binary, _/binary>> = Id) ->
-    Context = crossbar_doc:load(Id, cb_context:set_account_modb(Context0, kz_term:to_integer(Year), kz_term:to_integer(Month))),
+-spec mark_proforma_doc_deleted(cb_context:context(), ne_binary()) -> cb_context:context().
+mark_proforma_doc_deleted(Context, Id) ->
     AccountId = cb_context:account_id(Context),
-    Modb = kazoo_modb:get_modb(AccountId, kz_term:to_integer(Year), kz_term:to_integer(Month)),
-    case onbill_util:get_attachment(Id, Modb) of
-        {'ok', Attachment} ->
-            cb_context:set_resp_etag(
-                cb_context:set_resp_headers(cb_context:setters(Context
-                                                              ,[{fun cb_context:set_resp_data/2, Attachment}
-                                                               ,{fun cb_context:set_resp_etag/2, 'undefined'}
-                                                               ])
-                                            ,[{<<"Content-Disposition">>, <<"attachment; filename="
-                                                                            ,(kz_term:to_binary(Id))/binary
-                                                                            ,"-"
-                                                                            ,(kz_term:to_binary(Id))/binary
-                                                                            ,"-"
-                                                                            ,(kz_term:to_binary(Id))/binary>>
-                                              }
-                                             ,{<<"Content-Type">>, <<"application/pdf">>}
-                                             |cb_context:resp_headers(Context)
-                                           ])
-                ,'undefined'
-            );
-        _ ->
-            cb_context:add_system_error('faulty_request', Context0)
+    DbName = kz_util:format_account_id(AccountId,'encoded'),
+    case kz_datamgr:open_doc(DbName, Id) of
+        {ok, Doc} ->
+            kz_datamgr:ensure_saved(DbName, kz_json:set_value(<<"deleted_by_user">>, 'true', Doc)),
+            cb_context:set_resp_status(Context, 'success');
+        {'error', 'not_found'} ->
+            Context
     end.
--- src/onbill_maintenance.erl
+++ src/onbill_maintenance.erl
@@ -2,6 +2,7 @@
 
 -export([populate_modb_day_with_fee/4
          ,populate_modb_with_fees/3
+         ,refresh/0
         ]).
 
 -include("onbill.hrl").
@@ -13,4 +14,50 @@ populate_modb_with_fees(AccountId, Year, Month) ->
 -spec populate_modb_day_with_fee(ne_binary(), integer(), integer(), integer()) -> ok.
 populate_modb_day_with_fee(AccountId, Year, Month, Day) ->
     kz_bookkeeper_onbill:populate_modb_day_with_fee(kz_term:to_binary(AccountId), Year, Month, Day).
+
+-spec refresh() -> 'no_return'.
+-spec refresh(ne_binary() | nonempty_string()) -> 'ok' | 'remove'.
+-spec refresh(ne_binaries(), text() | non_neg_integer()) -> 'no_return'.
+-spec refresh(ne_binaries(), non_neg_integer(), non_neg_integer()) -> 'no_return'.
+refresh() ->
+    Databases = get_databases(),
+    refresh(Databases, 2 * ?MILLISECONDS_IN_SECOND).
+
+refresh(Databases, Pause) ->
+    Total = length(Databases),
+    refresh(Databases, kz_term:to_integer(Pause), Total).
+
+refresh([], _, _) -> 'no_return';
+refresh([Database|Databases], Pause, Total) ->
+    io:format("(~p/~p) refreshing database '~s'~n"
+             ,[length(Databases) + 1, Total, Database]),
+    _ = refresh(Database),
+    _ = case Pause < 1 of
+            'false' -> timer:sleep(Pause);
+            'true' -> 'ok'
+        end,
+    refresh(Databases, Pause, Total).
+
+-spec get_databases() -> ne_binaries().
+get_databases() ->
+        {'ok', Databases} = kz_datamgr:db_info(),
+            lists:sort(fun get_database_sort/2, lists:usort(Databases ++ ?KZ_SYSTEM_DBS)).
+
+-spec get_database_sort(ne_binary(), ne_binary()) -> boolean().
+get_database_sort(Db1, Db2) ->
+        kzs_util:db_priority(Db1) < kzs_util:db_priority(Db2).
+
+refresh(<<"onbill-", _/binary>> = DbName) ->
+    kz_datamgr:db_create(DbName),
+    _ = kz_datamgr:revise_doc_from_file(DbName, 'onbill', <<"views/docs_numbering.json">>),
+    'ok';
+refresh(DbName) when is_binary(DbName) ->
+    case kz_datamgr:db_classification(DbName) of
+        'account' ->
+            _ = kz_datamgr:revise_doc_from_file(DbName, 'onbill', <<"views/onbill_e911.json">>),
+            _ = kz_datamgr:revise_doc_from_file(DbName, 'onbill', <<"views/periodic_fees.json">>);
+        'modb' ->
+            _ = kz_datamgr:revise_doc_from_file(DbName, 'onbill', <<"views/onbills.json">>);
+        _Else -> 'ok'
+    end.
 
