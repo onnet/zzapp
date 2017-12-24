59b08ada0d4569d42e33ed7abaa2148ccfc1e81a
Kirill Sysoev <kirill.sysoev@gmail.com>
2017-12-24 18:54:27 +0000

    update

--- src/lb_tmp/onbill_lb.erl
+++ src/lb_tmp/onbill_lb.erl
@@ -1,11 +1,13 @@
 -module(onbill_lb).
 -author("Kirill Sysoev <kirill.sysoev@gmail.com>").
 
--export([
-     maybe_mysql_child/0
-]).
+-export([maybe_mysql_child/0
+        ,lbuid_by_uuid/1
+        ]).
 
 -include_lib("onbill.hrl").
+
+-define(LB_MYSQL_POOL, 'lb_mysql').
 
 -spec maybe_mysql_child() -> kz_proplists().
 maybe_mysql_child() ->
@@ -17,7 +19,17 @@ maybe_mysql_child() ->
                            ,{password, kapps_config:get_string(<<"onbill">>, <<"mysql_password">>, <<"password">>)}
                            ,{database, kapps_config:get_string(<<"onbill">>, <<"mysql_database">>, <<"database">>)}
                            ],
-            [mysql_poolboy:child_spec('lb_mysql', PoolOptions, MySqlOptions)];
+            [mysql_poolboy:child_spec(?LB_MYSQL_POOL, PoolOptions, MySqlOptions)];
         'false' ->
             []
+    end.
+
+-spec lbuid_by_uuid(ne_binary()) -> any().
+lbuid_by_uuid(AccountId) ->
+    case mysql_poolboy:query(?LB_MYSQL_POOL
+                            ,<<"select uid from accounts where uuid = ? limit 1">>
+                            ,[AccountId])
+    of
+        {ok,_,[[Uid]]} -> Uid;
+        _ -> 'undefined'
     end.
