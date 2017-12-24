8269845db7749e1d6fdc4047570eafb21bbaa4d2
Kirill Sysoev <kirill.sysoev@gmail.com>
2017-12-24 18:28:35 +0000

    update

--- src/lb_tmp/onbill_lb.erl
+++ src/lb_tmp/onbill_lb.erl
@@ -7,7 +7,17 @@
 
 -include_lib("onbill.hrl").
 
+-spec maybe_mysql_child() -> kz_proplists().
 maybe_mysql_child() ->
-    PoolOptions  = [{size, 10}, {max_overflow, 20}],
-    MySqlOptions = [{host, "host"}, {user, "zotonic"}, {password, "pwd"}, {database, "dbname"}],
-    [mysql_poolboy:child_spec(lb_mysql, PoolOptions, MySqlOptions)].
+    case kapps_config:get_is_true(<<"onbill">>, <<"mysql_pool_enable">>, 'false') of
+        'true' ->
+            PoolOptions  = [{size, 10}, {max_overflow, 20}],
+            MySqlOptions = [{host, kapps_config:get_string(<<"onbill">>, <<"mysql_host">>, <<"localhost">>)}
+                           ,{user, kapps_config:get_string(<<"onbill">>, <<"mysql_user">>, <<"user">>)}
+                           ,{password, kapps_config:get_string(<<"onbill">>, <<"mysql_password">>, <<"password">>)}
+                           ,{database, kapps_config:get_string(<<"onbill">>, <<"mysql_database">>, <<"database">>)}
+                           ],
+            [mysql_poolboy:child_spec('lb_mysql', PoolOptions, MySqlOptions)];
+        'false' ->
+            []
+    end.
