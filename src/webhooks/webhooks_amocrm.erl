c6c12e73e37d606baeff483d1e148e6db23b4f46
Kirill Sysoev <kirill.sysoev@gmail.com>
2018-04-21 21:10:07 +0000

    update

--- src/webhooks/webhooks_amocrm.erl
+++ src/webhooks/webhooks_amocrm.erl
@@ -46,34 +46,65 @@ bindings_and_responders() ->
 handle(JObj, _Props) ->
     'true' = kapi_call:event_v(JObj),
     AccountId = kz_json:get_value([<<"Custom-Channel-Vars">>, <<"Account-ID">>], JObj),
-    maybe_send_event(AccountId, format(JObj)).
+    maybe_handle_event(AccountId, format(JObj)).
 
--spec maybe_send_event(kz_term:api_binary(), kz_json:object()) -> 'ok'.
-maybe_send_event('undefined', _JObj) -> 'ok';
-maybe_send_event(AccountId, JObj) ->
+-spec maybe_handle_event(kz_term:api_binary(), kz_json:object()) -> 'ok'.
+maybe_handle_event('undefined', _JObj) -> 'ok';
+maybe_handle_event(AccountId, JObj) ->
   lager:info("IAM webhook JObj: ~p",[JObj]),
   lager:info("IAM webhook AccountId: ~p",[AccountId]),
     case webhooks_util:find_webhooks(?HOOK_NAME, AccountId) of
         [] ->
             lager:debug("no hooks to handle for ~s", [AccountId]);
         Hooks ->
-            lager:info("IAM webhook maybe_send_event Hooks: ~p",[Hooks]),
-            filter_hooks(Hooks)
+            filter_hooks(JObj, Hooks)
     end.
 
-filter_hooks([]) -> 'ok';
-filter_hooks([#webhook{hook_event = <<"amocrm">>
-                      ,custom_data = CustomData
-                      }=Hook|T]) ->
+handle_event(<<"CHANNEL_CREATE">>, JObj, Hook) ->
     lager:info("=================================================="),
-    lager:info("IAM print_hooks Hook: ~p", [Hook]),
+    lager:info("IAM print_hooks event_name: CHANNEL_CREATE"),
     lager:info("IAM print_hooks webhook.hook_event: ~p", [Hook#webhook.hook_event]),
-    lager:info("IAM print_hooks CustomData: ~p", [CustomData]),
     lager:info("=================================================="),
-    filter_hooks(T);
-filter_hooks([_|T]) ->
-    filter_hooks(T).
-    
+    {'ok', Cookie} = amocrm_auth_cookie(Hook),
+    contact_lookup(Cookie, JObj, Hook),
+    'ok';
+
+handle_event(<<"CHANNEL_ANSWER">>, JObj, Hook) ->
+    lager:info("=================================================="),
+    lager:info("IAM print_hooks event_name: CHANNEL_ANSWER"),
+    lager:info("IAM print_hooks webhook.hook_event: ~p", [Hook#webhook.hook_event]),
+    lager:info("=================================================="),
+    {'ok', Cookie} = amocrm_auth_cookie(Hook),
+    case contact_lookup(Cookie, JObj, Hook) of
+        {'ok', Contact} ->
+        {'ok', 'contact_not_found'} ->
+        {'error', 'contact_lookup_failed'} ->
+            'ok'
+    end;
+
+handle_event(<<"CHANNEL_DESTROY">>, _JObj, Hook) ->
+    lager:info("=================================================="),
+    lager:info("IAM print_hooks event_name: CHANNEL_DESTROY"),
+    lager:info("IAM print_hooks webhook.hook_event: ~p", [Hook#webhook.hook_event]),
+    lager:info("=================================================="),
+    'ok'.
+
+compose_note(JObj, Contact) ->
+    ElementTypes = [{<<"contact">>, 1}
+                   ,{<<"lead">>, 2}
+                   ,{<<"company">>, 3}
+                   ,{<<"task">>, 4}
+                   ],
+    Values = [{<<"element_id">>, kz_json:get_value(<<"id">>, Contact)}
+             ,{<<"element_type">>, kz_json:get_value(kz_json:get_value(<<"type">>, Contact), ElementTypes, 1)}
+             ],
+
+filter_hooks(_, []) -> 'ok';
+filter_hooks(JObj, [#webhook{hook_event = <<"amocrm">>}=Hook|T]) ->
+    handle_event(kz_json:get_value(<<"event_name">>, JObj), JObj, Hook),
+    filter_hooks(JObj, T);
+filter_hooks(JObj, [_|T]) ->
+    filter_hooks(JObj, T).
 
 -spec format(kz_json:object()) -> kz_json:object().
 format(JObj) ->
@@ -87,3 +118,66 @@ format(JObj) ->
               %   ,<<"Custom-Channel-Vars">>
                  ],
     kz_json:normalize_jobj(JObj1, RemoveKeys, []).
+
+amocrm_auth_cookie(#webhook{uri = URL
+                           ,custom_data = CustomData
+                           }
+                  ) ->
+    UserLogin = kz_json:get_first_defined([<<"USER_LOGIN">>, <<"user_login">>], CustomData),
+    UserHash = kz_json:get_first_defined([<<"USER_HASH">>, <<"user_hash">>], CustomData),
+    case kz_http:post(<<URL/binary, "/private/api/auth.php?type=json">>
+                     ,[]
+                     ,{[{<<"USER_LOGIN">>,UserLogin},{<<"USER_HASH">>,UserHash}]})
+    of
+        {'ok', 200, Headers, _Body} ->
+            lager:info("IAM amocrm_auth: Headers, ~p",[Headers]),
+            Cookie = props:get_value("set-cookie", Headers),
+            lager:info("IAM amocrm_auth: Cookie, ~p",[Cookie]),
+            {'ok', Cookie};
+        {_, Code, Headers, Body} ->
+            lager:info("IAM amocrm_auth Failed Code: ~p",[Code]),
+            lager:info("IAM amocrm_auth Failed Headers: ~p",[Headers]),
+            lager:info("IAM amocrm_auth Failed Body: ~p",[Body]),
+            {'error', 'auth_failed'}
+    end.
+
+caller_number(JObj) ->
+    kz_json:get_first_defined([<<"caller_id_number">>], JObj).
+
+contact_lookup(Cookie, JObj, #webhook{uri = URL}) ->
+    CallerNumber = caller_number(JObj),
+    lager:info("IAM contact_lookup CallerNumber: ~p",[CallerNumber]),
+    case kz_http:get(<<URL/binary, "/private/api/v2/json/contacts/list?query=", CallerNumber/binary>>
+                    ,[{<<"Cookie">>, Cookie}]
+                    )
+    of
+        {'ok', 200, _Headers, Body} ->
+            lager:info("IAM contact_lookup successful Body: ~p",[kz_json:decode(Body)]),
+            [Contact|_] = kz_json:get_value([<<"response">>,<<"contacts">>], kz_json:decode(Body)),
+            lager:info("IAM contact_lookup successful Contact: ~p",[Contact]),
+            {'ok', Contact};
+        {'ok', 204, _Headers, _Body} ->
+            lager:info("IAM contact_lookup returned empty result."),
+            {'ok', 'contact_not_found'};
+        {_, Code, Headers, Body} ->
+            lager:info("IAM contact_lookup Failed Code: ~p",[Code]),
+            lager:info("IAM contact_lookup Failed Headers: ~p",[Headers]),
+            lager:info("IAM contact_lookup Failed Body: ~p",[Body]),
+            {'error', 'contact_lookup_failed'}
+    end.
+
+set_note(Cookie, DataBag, #webhook{uri = URL}) ->
+    case kz_http:post(<<URL/binary, "/private/api/auth.php?type=json">>
+                     ,[{<<"Cookie">>, Cookie}]
+                     ,DataBag
+                     )
+    of
+        {'ok', 200, _Headers, Body} ->
+            lager:info("IAM set_note successful Body: ~p",[kz_json:decode(Body)]),
+            ok;
+        {_, Code, Headers, Body} ->
+            lager:info("IAM set_note Failed Code: ~p",[Code]),
+            lager:info("IAM set_note Failed Headers: ~p",[Headers]),
+            lager:info("IAM set_note Failed Body: ~p",[Body]),
+            {'error', 'set_note_failed'}
+    end.
